d483fbb53cb9685f1139f1153037625ae043e0eb
Chandu96k <125717410+Chandu96k@users.noreply.github.com>
2023-02-27 13:52:13 +0530

    Add files via upload

---
+++ sumOfEven.java
@@ -0,0 +1,28 @@
+package BASIC_PROGRAMS;
+
+import java.util.Scanner;
+
+public class sumOfEven {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENTER A M NO ");
+		int m =sc.nextInt();
+		System.out.println("ENTER A N NO ");
+		int n=sc.nextInt();
+		
+		while(m<=n)
+		{
+			if((m%2)==0)
+			{
+				System.out.println(m);
+				m++;
+			}
+			else
+				System.exit(0);
+		}
+
+	}
+
+}
